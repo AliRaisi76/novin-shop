d7fc79cf552858919247540275962c7759ebb56e
AliRaisi76 <aliraisi97@outlook.com>
2024-01-04 01:06:55 +0330

    Added the model product

--- models/Product.js
+++ models/Product.js
@@ -1,3 +1,20 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'The product must have a title!'],
+        maxLength: 50
+    },
+    description: {
+        type: String,
+        required: [true, 'A product must have a description!'],
+        maxLength: 500
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Please add how much of this product is in stock!']
+    }
+});
+
+module.exports = mongoose.Model('Product', productSchema)
