9684c128f01e1e4274a03598672ce79f8a1e53ac
Ali Raisi <aliraisi76@gmail.com>
2024-01-14 02:37:10 +0330

    Added regiester user with jwt token assigned to the response

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,19 @@
 const User = require('../models/userModel')
 const asyncHandler = require('../middleware/asyncHandler')
+
 // @desc Register user
 // @route Post /api/v1/users
 // @access Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { email, password, role } = req.body
   let user = await User.create({ email, password, role })
+
+  // Create token
+  const token = user.getSignedJwtToken()
+
   res.status(200).json({
+    success: true,
+    token,
     data: user,
   })
 })
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   email: {
@@ -30,5 +32,21 @@ const userSchema = mongoose.Schema({
     default: Date.now,
   },
 })
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(12)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+// Sign jwt token
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  })
+}
 
 module.exports = new mongoose.model('user', userSchema)
