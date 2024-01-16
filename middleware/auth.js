014d14e19964275bb64731f87fbd33030b5bd32c
Ali Raisi <aliraisi76@gmail.com>
2024-01-17 01:01:42 +0330

    Added route protecctor middleware for authorization

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('./asyncHandler')
+const ErrorResponse = require('../utils/ErrorResponse')
+const User = require('../models/userModel')
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  } else if (req.cookies.token) {
+    // Use cookies to get the token from the front-end
+    token = req.cookies.token
+  }
+
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to acces this route!', 401))
+  }
+
+  try {
+    // Verify tokne
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded)
+    req.user = await User.findById(decoded.id)
+    next()
+  } catch (err) {
+    return next(new ErrorResponse('Not authorized to acces this route!', 401))
+  }
+})
