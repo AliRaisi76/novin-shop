c26656a8db5b60a5e789f9770fa443c94bb2ac85
Ali Raisi <aliraisi76@gmail.com>
2024-01-17 04:24:00 +0330

    Added authorize middleware for roles accessbility

--- middleware/auth.js
+++ middleware/auth.js
@@ -31,3 +31,17 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Not authorized to acces this route!', 401))
   }
 })
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route!`
+        ),
+        403
+      )
+    }
+    next()
+  }
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 const { register, login, getMe } = require('../controllers/authController')
 
