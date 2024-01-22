591972e2b568ec87199b6c6b9fab9d9330feb9e9
Ali Raisi <aliraisi76@gmail.com>
2024-01-23 00:14:32 +0330

    Upgraded user model!

--- models/userModel.js
+++ models/userModel.js
@@ -4,26 +4,89 @@ const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 
 const userSchema = mongoose.Schema({
+  personalInfo: {
+    firstName: {
+      type: String,
+      trim: true,
+      maxlength: 50,
+      match: [/^[a-zA-Z]+$/, 'Provide a valid first name!'],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxlength: 50,
+      match: [/^[a-zA-Z]+$/, 'Provide a last name!'],
+    },
+    nationalCode: {
+      type: String,
+      match: [/^\d{10}$/, 'Please add a valid national code'],
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+      match: [/^09\d{9}$/, 'Please provide a Valid Iranian phone number!'],
+    },
+    birthDate: {
+      type: Date,
+    },
+  },
   email: {
     type: String,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email',
     ],
+    required: [true, 'please provide an email!'],
+    unique: true,
   },
   password: {
     type: String,
     required: [true, 'Please provide a password!'],
     minlength: 6,
     select: false,
+    match: [
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{6,}$/,
+      'Please provide a strong password!',
+    ],
+    maxlength: 50,
   },
-  address: {
-    type: String,
-    // required: [true, 'Please add an address'],
-  },
+  locations: [
+    {
+      address: {
+        type: String,
+        maxlength: 500,
+        trim: true,
+      },
+      city: {
+        type: String,
+        trim: true,
+        match: [/^[a-zA-Z]+$/, 'Provide a valid city name!'],
+      },
+      province: {
+        type: String,
+        trim: true,
+        match: [/^[a-zA-Z]+$/, 'Provide a valid province name!'],
+      },
+      postalCode: {
+        type: String,
+        trim: true,
+        match: [/^\d{10}$/, 'Please add a valid postal code'],
+      },
+      recipient: {
+        type: String,
+        trim: true,
+        match: [/^[a-zA-Z]+$/, 'Provide a valid recipient full name!'],
+      },
+      recipientPhoneNumber: {
+        type: String,
+        match: [/^09\d{9}$/, 'Please provide a Valid Iranian phone number!'],
+      },
+    },
+  ],
   role: {
     type: String,
-    enum: ['customer'],
+    enum: ['customer', 'manager'],
     default: 'customer',
   },
   resetPasswordToken: String,
